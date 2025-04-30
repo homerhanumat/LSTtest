46d513bee22ee95da5d1d3b66e9bcb0e01770200
Homer White <homerhanumat@gmail.com>
2025-04-30 07:49:16 -0400

    test emptying optional packages vector

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 # For the user's convenience, attach packages that are often used in the textbook
 
 pkgs_to_attach <- c("ggplot2", "dplyr")
-optional_packages <- c("mosaicData", "moderndive",
-                       "palmerpenguins")
+#optional_packages <- c("mosaicData", "moderndive", "palmerpenguins")
+optional_packages <- character()
 # if an optional package is installed, .onLoad() will attach it.
 add_to_attach <- function(pkg) {
   if (requireNamespace(pkg, quietly = TRUE))
